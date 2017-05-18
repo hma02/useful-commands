1a078acb2e32e93d96b5126fefec4555ccc444fc
He <hma02@users.noreply.github.com>
2017-05-18 13:06:59 -0400

    move dir selectively based on dir size

---
+++ 14.find-and-move-dir-smaller-than-a-size.sh
@@ -0,0 +1,21 @@
+echo "Type an upper limit as the size of the dir to be found and moved, followed by [ENTER]:"
+read size
+
+
+if [[ ! -d dest_dir ]]
+then
+	mkdir dest_dir
+fi
+
+find ./ -mindepth 1 -maxdepth 1 -type d -exec du -s {} + | awk '$1 <= '"$size"' {print $1, $2}'
+
+
+echo "Continue moving? type yes of no, followed by [ENTER]:"
+read decision
+if [[ "$decision" == "yes" ]]
+then
+	find ./ -mindepth 1 -maxdepth 1 -type d -exec du -s {} + | awk '$1 <= '"$size"' {print $2}' | while read f; do mv "$f" ./dest_dir; done
+	echo 'move complete'
+else
+	echo 'move cancelled'
+fi
